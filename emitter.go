21fb49eaf56653e7d5db0d086571743c10c153eb
liusu <2>
2022-10-08 11:56:19 +0800

    调整GC

--- emitter.go
+++ emitter.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/coocood/freecache"
 	"io"
 	"perfma-replay/byteutils"
 	"perfma-replay/message"
@@ -30,11 +29,10 @@ func Start(stop chan int) {
 
 // CopyMulty copies from 1 reader to multiple writers
 func CopyMulty(src message.PluginReader, writers ...message.PluginWriter) (err error) {
-	if Settings.CopyBufferSize < 1 {
-		Settings.CopyBufferSize = 5 << 20
-	}
+	filteredCount := 0
 	modifierRule := modifier.NewHTTPModifier(&Settings.modifierConfig)
-	filteredRequests := freecache.NewCache(200 * 1024 * 1024) // 200M
+	filteredRequestsLastCleanTime := time.Now().UnixNano()
+	filteredRequests := make(map[string]int64)
 	for {
 		msg, er := src.PluginReader()
 		if er != nil {
@@ -51,21 +49,22 @@ func CopyMulty(src message.PluginReader, writers ...message.PluginWriter) (err e
 				Debug(2, fmt.Sprintf("[EMITTER] Found malformed record %q from %q", msg.Meta, src))
 				continue
 			}
+			requestID := byteutils.SliceToString(meta[1])
 			if Settings.Verbose >= 3 {
 				Debug(3, "[EMITTER] input: ", byteutils.SliceToString(msg.Meta[:len(msg.Meta)-1]), " from: ", src)
 			}
-			requestID := meta[1]
 			if(modifierRule != nil){
 				if(proto.IsRequestPayload(msg.Meta)){
 					msg.Data = modifierRule.Rewrite(msg.Data)
 					if(len(msg.Data) == 0){
-						filteredRequests.Set(requestID, []byte{}, 60) //
+						filteredRequests[requestID] = time.Now().UnixNano()
+						filteredCount++
 						continue
 					}
 				}else{
-					_, err := filteredRequests.Get(requestID)
-					if err == nil {
-						filteredRequests.Del(requestID)
+					if _, ok := filteredRequests[requestID]; ok {
+						delete(filteredRequests, requestID)
+						filteredCount--
 						continue
 					}
 				}
@@ -75,6 +74,20 @@ func CopyMulty(src message.PluginReader, writers ...message.PluginWriter) (err e
 				if _, err := dst.PluginWriter(msg); err != nil && err != io.ErrClosedPipe {
 					return err
 				}
+			}
+		}
+		// Run GC on each 1000 request
+		if filteredCount > 0 && filteredCount%1000 == 0 {
+			// Clean up filtered requests for which we didn't get a response to filter
+			now := time.Now().UnixNano()
+			if now-filteredRequestsLastCleanTime > int64(60*time.Second) {
+				for k, v := range filteredRequests {
+					if now-v > int64(60*time.Second) {
+						delete(filteredRequests, k)
+						filteredCount--
+					}
+				}
+				filteredRequestsLastCleanTime = time.Now().UnixNano()
 			}
 		}
 	}
